e6abdcfabce0974c03c369c360756315a6b3edc0
Inposa <donhippo04@gmail.com>
2020-04-01 15:21:25 +0200

    add a mock to test event for billing.js

--- billing.js
+++ billing.js
@@ -6,10 +6,8 @@ export async function main(event, context) {
   const { storage, source } = JSON.parse(event.body);
   const amount = calculateCost(storage);
   const description = "Scratch charge";
-
   // Load our secret key from the  environment variables
   const stripe = stripePackage(process.env.stripeSecretKey);
-
   try {
     await stripe.charges.create({
       source,
